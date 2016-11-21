eeec04069dbcd4f1f17e989ed54884af10d5a9fc
olavobm <olavobm@gmail.com>
2016-11-21 15:08:39 -0800

    Commiting very inefficient version.

---
+++ obmMisc/sepColsNanPos.m
@@ -0,0 +1,81 @@
+function colsets = sepColsNanPos(x)
+%
+%   inputs:
+%       - x: matrix
+%
+%   outputs:
+%       - colsets: cell array with the column indices of each set....
+%
+% Function SEPCOLSNANPOS
+%
+% Olavo Badaro Marques, 21/Nov/2016.
+
+%%
+allcols = 1:size(x, 2);
+
+indnan = find(isnan(x));
+% indok = find(~isnan(x));    % maybe I could change if
+                              % it increases efficiency.
+            
+[rnan, cnan] = ind2sub(size(x), indnan);
+             
+
+cnanUniq = unique(cnan);
+
+cnanUniqCopy = cnanUniq;
+
+
+%%
+% Pre-allocate (likely extra) space:
+colsets = cell(1, length(cnanUniq));
+
+indfill = 1;
+
+while length(cnanUniqCopy)>1
+    
+    caux = cnanUniqCopy(1);
+    raux = rnan(cnan==caux);
+    
+    colsets{indfill} = [colsets{indfill}, caux];
+    
+%     colsets_aux = cell(1, length(cnanUniqCopy));
+%     colsets_aux{1} = caux;
+    
+    for i = 2:length(cnanUniqCopy)
+        
+        if isequal(raux, rnan(cnan==cnanUniqCopy(i)))
+            
+            colsets{indfill} = [colsets{indfill}, cnanUniqCopy(i)];
+            
+        end
+    end
+    
+    
+    cnanUniqCopy = setdiff(cnanUniqCopy, colsets{indfill});
+    indfill = indfill + 1;
+    
+    % Print message to the screen:
+    disp(['There are ' num2str(length(cnanUniqCopy)) ' columns remaining'])
+    
+end
+
+if length(cnanUniqCopy)==1
+    colsets{indfill} = [colsets{indfill}, cnanUniqCopy];
+    indfill = indfill + 1;
+end
+
+
+%% Get rid of empty columns at the end:
+colsets_aux = colsets(1:(indfill-1));
+
+% should work the same as colsets = colsets(~ismepty(colsets))
+if ~isequal(colsets_aux, colsets(~cellfun(@isempty, colsets)))
+    error('Function sepColsNanPos has unexpected error.')
+end
+
+colsets = colsets_aux;
+
+
+%% Add column indices of the columns with no NaNs:
+
+colsets = [setdiff(allcols, cnanUniq), colsets];
