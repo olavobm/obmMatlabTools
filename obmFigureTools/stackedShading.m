6d525db1e301cc4b7009595f5e9aafbce0147837
olavobm <olavobm@gmail.com>
2016-11-29 17:21:32 -0800

    Initial commit.

---
+++ obmFigureTools/stackedShading.m
@@ -0,0 +1,11 @@
+function stackedShading(x, y)
+% STACKEDSHADING(x, y)
+%
+%	inputs:
+%       - x: vector
+%       - y: array number of columns
+%
+% Olavo Badaro Marques, 29/Nov/2016.
+
+
+%% Create color scheme
