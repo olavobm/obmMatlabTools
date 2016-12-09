b6cb4f44a523ee21342ba57e64b76105c6773b54
olavobm <olavobm@gmail.com>
2016-12-09 15:39:23 -0800

    setFigSize.m: initial commit.

---
+++ obmFigureTools/setFigSize.m
@@ -0,0 +1,59 @@
+function setFigSize(lenhgt, units)
+% SETFIGURESIZE(lenhgt, units)
+%
+%   inputs:
+%       - lenhgt: length-height vector. lenhgt(1)/lenhgt(2) is the
+%                 length/height of the figure.
+%       - units (optional): figure size units. The default is centimeters.
+%                           The options are: 'inches'; 'centimeters';
+%                           'characters'; 'normalized'; 'points'; 'pixels'.
+%
+% The resizing of Matlab's figure window as it appear on the screen is a
+% copy of the function wysiwyg.m, created by Dan(K) Braithwaite and
+% available on the web.
+%
+% Suggestions: include figure handle in the input
+%
+% Olavo Badaro Marques, 09/Dec/2016.
+
+
+%%
+
+hFig = gcf;
+
+
+%%
+
+if ~isempty(units)
+    units = 'centimeters';
+end
+
+
+%%
+
+if length(lenhgt)~=2
+    error('Input lenhgt must have 2 elements only.')
+else
+    if iscolumn(lenhgt)
+        lenhgt = lenhgt';
+    end
+end
+
+
+%%
+
+set(hFig, 'Units', units)
+set(hFig, 'PaperSize', lenhgt)
+set(hFig, 'PaperPosition', [0 0 lenhgt])
+
+
+%% Resize Matlab's figure window function 
+% wysiwyg.m
+% 
+%     unis = get(gcf,'units');
+%     ppos = get(gcf,'paperposition');
+%     set(gcf,'units',get(gcf,'paperunits'));
+%     pos = get(gcf,'position');
+%     pos(3:4) = ppos(3:4);
+%     set(gcf,'position',pos);
+%     set(gcf,'units',unis);
