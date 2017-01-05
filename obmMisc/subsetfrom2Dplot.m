4134f5e92db165464d2ec948f343103a7d46a4d8
olavobm <olavobm@gmail.com>
2017-01-05 10:21:02 -0800

    initial commit of subsetin2D.m and subsetfrom2Dplot.m.

---
+++ obmMisc/subsetfrom2Dplot.m
@@ -0,0 +1,81 @@
+function [zpts, xygpts, xypts] = subsetfrom2Dplot(axishandle, surfacehandle)
+% [zpts, xygpts, xypts] = SUBSETFROM2DPLOT(axishandle)
+%
+%   inputs:
+%       - axishandle: axes handle of the figure you want to subset.
+%       - surfacehandle (optional):
+%
+%   outputs:
+%       - zpts: subsetted values of input z.
+%       - xygpts: values of (x, y) associated with zpts,
+%                 i.e. the closest (x, y) to xypts.
+%
+% SUBSETFROM2DPLOT lets you click as many times as you want inside the
+% axes (with handle axishandle) and gets the value of the plotted
+% variable in the nearest locations where the variable is defined.
+%
+% When SUBSETFROM2DPLOT is called, a loop is opened. To complete the
+% execution, click on the figure outside the axes limits.
+%
+% Olavo Badaro Marques, 04/Jan/2017.
+
+% IF THERE ARE MULTIPLE PLOTS OVERLAYED, THIS FUNCTION WON'T WORK.
+% I SHOULD COME UP WITH A WAY TO FIX IT. I COULD ADD AN OPTIONAL INPUT
+% TO SPECIFY THE HANDLE OF THE PLOT...SOUNDS GOOD TO ME.
+
+
+%% Bring forward the window of the
+% figure that cointains axishandle:
+
+axes(axishandle)
+
+
+%% Get limits of the axes:
+
+xaxislims = get(axishandle, 'XLim');
+yaxislims = get(axishandle, 'YLim');
+
+
+%% Open a while loop to select points on the plot. Terminate
+% the loop by clicking outside the axes limits:
+
+linwhile = true;
+
+xypts = [];
+
+while linwhile
+    
+    xyptaux = ginput(1);
+    
+    linxlim = xyptaux(1)>=xaxislims(1) && xyptaux(1)<=xaxislims(2);
+    linylim = xyptaux(2)>=yaxislims(1) && xyptaux(2)<=yaxislims(2);
+    
+    % If xyptaux is within axes limits, concatenate to xypts array:
+    if linxlim && linylim
+        
+        xypts = [xypts; xyptaux];    %#ok<AGROW>
+        % the comment above supresses the warning
+        % message on Matlab's text editor
+        
+	% Otherwise, terminates while loop:
+    else
+        linwhile = false;
+    end
+    
+end
+
+
+%% Get x, y, z values from the children of
+% the axes and subset acording to xpts:
+
+if ~exist('surfacehandle', 'var')
+    plthandle = get(axishandle, 'Children');
+else
+    plthandle = surfacehandle;
+end
+
+
+[zpts, xygpts] = subsetin2D(xypts, ...
+                            plthandle.XData, plthandle.YData, plthandle.CData);
+
+
---
+++ obmMisc/subsetin2D.m
@@ -0,0 +1,64 @@
+function [zpts, xygpts] = subsetin2D(xypts, x, y, z, rlim)
+% [zpts, xypts] = subsetin2D(xypts, x, y, z, rlim)
+%
+%   inputs:
+%       - xypts: Nx2 array.
+%       - x:
+%       - y:
+%       - z:
+%       - rlim (optional): radius 
+%
+%   outputs:
+%       - zpts: subsetted values of input z.
+%       - xygpts: values of (x, y) associated with zpts,
+%                 i.e. the closest (x, y) to xypts.
+%
+% For a matrix z, whose values are speficied at (x, y), SUBSETIN2D
+% z at the points xypts. The subsetting is done by a Matlab function
+% called dsearchn.
+% 
+% If you have a plot of z, you may as well call the function
+% subsetfrom2Dplot.m which uses ginput to get the point xypts.
+%
+% Olavo Badaro Marques, 04/Jan/2017.
+
+% SUGGESTIONS of things to do:
+%           - rlim could be a vector.
+%           - x and y matrices or vectors that
+%             already contain all grid points.
+%           - I could also include interpolation to get
+%             values right on top ox xypts
+
+
+%% Check whether optional input rlim was specified:
+
+if ~exist('rlim', 'var')
+    rlim = NaN;    
+end
+
+
+%% If x and y are vectors, assume the grid is regular
+% and we create it so we can subset z on it:
+
+if isvector(x) && isvector(y)
+    [xg, yg] = meshgrid(x, y);
+    xyall = [xg(:), yg(:)];
+end
+
+
+%% Subset z at the closest (x, y) to xypts:
+
+[indpts, distpts] = dsearchn(xyall, xypts);
+
+zpts = z(indpts);
+xygpts = xyall(indpts, :);
+
+
+%% If rlim was specified, then NaN z values that
+% are outside the circle of radius rlim:
+
+if ~isnan(rlim)
+	ltoofar = distpts > rlim;
+    zpts(ltoofar) = NaN;
+end
+
