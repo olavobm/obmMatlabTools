43e25f8fe435b9da1299bf141236036852986bcd
olavobm <olavobm@gmail.com>
2017-01-20 14:44:38 -0800

    minor additions.

--- obmSpectra/spectraRows.m
+++ obmSpectra/spectraRows.m
@@ -1,9 +1,40 @@
-function pwspec = spectraRows(x, dt, rowstep)
-%
+function pwspec = spectrarows(x, dt, np, indrows, ovrlap)
+% pwspec = SPECTRAROWS(x, dt, rowstep)
 %
 %   inputs:
-%
-%
-%
+%       - x:
+%       - dt:
+%       - np:
+%       - indrows (optional): indices of the rows to operate
+%                             on. Default is all rows.
+%       - ovrlap (optional):
 %
 %   outputs:
+%       - pwspec:
+%
+% Function SPECTRAROWS computes power spectra, one for every
+% row of x, using the function obmPSpec.m.
+%
+% Olavo Badaro Marques, 19/Jan/2017.
+
+
+%% Check if indrows optional input was specified. Since
+% one may give ovrlap but specify indrows as an empty
+% array, also use default value for this case:
+
+if ~exist('indrows', 'var')
+    indrows = 1:size(x, 1);
+else
+    if isempty(indrows)
+        indrows = 1:size(x, 1);
+    end
+end
+
+
+%%
+
+for i = 1:length(indrows)
+    
+    pwspec = obmPSpec(x(indrows(i), :), dt, np);
+    
+end
