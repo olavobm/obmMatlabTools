d7ae9e57036ac9968d7d78c5076e6df1cf98c4ee
olavobm <olavobm@gmail.com>
2017-01-20 16:20:52 -0800

    looks good. So far, it has no code to deal with gaps.

--- obmSpectra/spectrarows.m
+++ obmSpectra/spectrarows.m
@@ -1,19 +1,24 @@
 function pwspec = spectrarows(x, dt, np, indrows, ovrlap)
-% pwspec = SPECTRAROWS(x, dt, rowstep)
+% pwspec = SPECTRAROWS(x, dt, np, indrows, ovrlap)
 %
 %   inputs:
-%       - x:
-%       - dt:
-%       - np:
+%       - x: matrix for which we will compute power spectra of its rows.
+%            As a reminder, the columns must represent uniform spacing.
+%       - dt: sampling period (distance between columns).
+%       - np: number of points 
 %       - indrows (optional): indices of the rows to operate
 %                             on. Default is all rows.
-%       - ovrlap (optional):
+%       - ovrlap (optional): overlap between chuncks (number
+%                            between 0-1, default is 0.5).
 %
 %   outputs:
 %       - pwspec:
 %
 % Function SPECTRAROWS computes power spectra, one for every
 % row of x, using the function obmPSpec.m.
+%
+% This function also calls createEmptyStruct.m to organize
+% the output into a structure array.
 %
 % Olavo Badaro Marques, 19/Jan/2017.
 
@@ -31,10 +36,42 @@ else
 end
 
 
-%%
+%% Check if ovrlap input exists. If not, assign default value:
 
-for i = 1:length(indrows)
+if ~exist('ovrlap', 'var')
+    ovrlap = 0.5;
+end
+
+
+%% Take the power spectrum for each indrows(i)
+% row of x. The if inside the loop organizes
+% the output into a structure array:
+
+% Number of rows where power spectrum is taken:
+nrows = length(indrows);
+
+% Loop over rows:
+for i = 1:nrows
     
-    pwspec = obmPSpec(x(indrows(i), :), dt, np);
+    % Compute power spectrum:
+	pwspecaux = obmPSpec(x(indrows(i), :), dt, np, 0, ovrlap, 0);
+    
+    % If the it is the first iteration, get the field names
+    % of the output of obmPSpec to create an empty struct
+    % array (to be filled at each iteration):
+    if i==1
+        
+        % Create empty structure and assign power spectrum
+        % structure to the first entry in the array:
+        pwspec = createEmptyStruct(fieldnames(pwspecaux), nrows);
+        
+        pwspec(1) = pwspecaux;
+        
+    else
+        
+        % Assign power spectrum structure to the output array:
+        pwspec(i) = pwspecaux;
+        
+    end
     
 end
