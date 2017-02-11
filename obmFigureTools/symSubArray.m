8f04b565944cb3a317374207b08796a04982d0d3
olavobm <olavobm@gmail.com>
2017-02-10 19:51:48 -0800

    first commit.

---
+++ obmFigureTools/symSubArray.m
@@ -0,0 +1,14 @@
+function symSubArray(mxy, mixy, nxy, lmake)
+% SYMSUBARRAY(mxy, mixy, nxy, lmake)
+%
+%   inputs:
+%       - mxy: 11x1 or 1x2 array.
+%       - mixy:
+%       - nxy:
+%       - lmake:
+%
+% SYMSUBARRAY calls makeSubPlots.m to create a new figure with an
+% array of subplots. The simplification of SYMSUBARRAY is that opposite
+% margins (or all of them) have the same length.
+%
+% Olavo Badaro Marques, 10/Feb/2017.
