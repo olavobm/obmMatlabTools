bc14715a5a59b20d0e9a0b89f9a3afac7df2d2bc
olavobm <olavobm@gmail.com>
2017-03-07 18:42:35 -0800

    minor changes and more comments.

--- obmFigureTools/linkallaxes.m
+++ obmFigureTools/linkallaxes.m
@@ -1,17 +1,21 @@
 function linkallaxes(axsstr)
+% LINKALLAXES(axsstr)
 %
 %   inputs:
-%       -
+%       - axsstr: string in the same manner as it goes into linkaxes.m
 %
+% LINKALLAXES applys linkaxes.m for all axes in the current
+% figure (so you don't have to specify all axes handles).
 %
 % Olavo Badaro Marques, 07/Mar/2017.
 
 
+%% Get all axes handles in the current figure:
 
-%%
-% allAxes = findall(hfig(i), 'Type', 'axes');
 allAxes = findall(gcf, 'Type', 'axes');
 onlyAxes = allAxes(~ismember(get(allAxes, 'Tag'), {'legend', 'Colobar'}));
 
-%%
+
+%% Call linkaxes:
+
 linkaxes(onlyAxes, axsstr)
--- obmFigureTools/titleAll.m
+++ obmFigureTools/titleAll.m
@@ -2,34 +2,53 @@ function titleAll(celltitles, axeshandles, varargin)
 % TITLEALL(celltitles, varargin)
 %
 %   inputs:
-%       - celltitles:
-%       - axeshandles (optional):
-%       - varargin (optional):
+%       - celltitles: cell array of strings to be subplot titles.
+%       - axeshandles (optional): array of axes handles referencing to
+%                                 the same axes as celltitles.
+%       - varargin (optional): parameter name-value pairs to
+%                              customize title.
 %
+% Add title to all subplots in current figure. If not specifying input
+% axeshandles, the order of celltitles should be given in the same
+% order in which the existing axes were created.
 %
 % Olavo Badaro Marques, 07/Mar/2017.
 
-allAxes = findall(gcf, 'Type', 'axes');
-onlyAxes = allAxes(~ismember(get(allAxes, 'Tag'), {'legend', 'Colobar'}));
 
-if length(onlyAxes) ~= length(celltitles)
-    error('')
+%% If axeshandles is not given, get all existing axes in current figure:
+
+if ~exist('axeshandles', 'var') || isempty(axeshandles)
+    
+    allAxes = findall(gcf, 'Type', 'axes');
+    onlyAxes = allAxes(~ismember(get(allAxes, 'Tag'), {'legend', 'Colobar'}));
+    
+    axeshandles = flipud(onlyAxes);  % flipud because matlab "store" axes
+                                     % such that the first handle
+                                     % references to the last one that
+                                     % was created
+end
+
+
+%% Get axes handles in current figure:
+
+if length(axeshandles) ~= length(celltitles)
+    error(['Input celltitles does NOT have the same length as ' ...
+           'the axeshandles array. It must have.'])
 else
     n = length(celltitles);
 end
 
 
-%%
-
-if ~exist('axeshandles', 'var') || isempty(axeshandles)
-    
-    axeshandles = flipud(onlyAxes);
-end
-
-%%
+%% Loop over axes and title them:
 
 for i = 1:n
-    axes(axeshandles(i))
-    title(celltitles{i}, varargin{:})
+%     axes(axeshandles(i))   % Matlab warns this can be slow...
+    axeshandles(i).Title.String = celltitles{i};
+    
+%     for i2 = 1:(length(varargin)/2)
+%         axeshandles(i2).Title.() = celltitles{i};
+%     end
+    
+%     title(celltitles{i}, varargin{:})
 end
 
