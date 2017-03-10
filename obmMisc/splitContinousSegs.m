298529b954dcd8da02ded9fd3d21b102d8bc9eb8
olavobm <olavobm@gmail.com>
2017-03-10 12:43:30 -0800

    minor changes and more comments.

--- obmMisc/splitContinousSegs.m
+++ obmMisc/splitContinousSegs.m
@@ -1,45 +1,55 @@
-function segscell = splitContinousSegs(inds)
-% segscell = SPLITCONTINOUSSEGS(inds)
+function segscell = splitContinousSegs(x)
+% segscell = SPLITCONTINOUSSEGS(x)
 %
 %   inputs:
-%       - inds:
+%       - x: vector of integers in ascending order.
 %
 %   outputs:
-%       - segscell:
+%       - segscell: 1xN cell array where each element
+%                   is a continuous segment of x.
 %
+% SPLITCONTINOUSSEGS splits the vector into its (N) continuous
+% segments. Discontinuities are defined by the locations in x
+% where subsequent elements differ by more than 1.
 %
 % Olavo Badaro Marques, 10/Mar/2017.
 
 
-%%
+%% Identify discontinuities:
 
-diffinds = diff(inds);
+xdiff = diff(x);
 
-indsplit = find(diffinds > 1);
+indsplit = find(xdiff > 1);
 
 
-%%
+%% Split continuous segments of x:
 
+% Number continuous segments:
 n = length(indsplit) + 1;
 
+% Pre-allocate space for output:
 segscell = cell(1, n);
 
-
+% Loop over all segments and get
+% the correspondent elements of x:
 for i = 1:n
 
+    % Take start index of the i'th segment:
     if i==1
         indauxstart = 1;
     else
         indauxstart = indsplit(i-1) + 1;
     end
 
+    % Take end index of the i'th segment:
     if i<n
         indauxend = indsplit(i);
     else
-        indauxend = length(inds);
+        indauxend = length(x);
     end
     
-    segscell{i} = inds(indauxstart:indauxend);
+    % Assign segment to output:
+    segscell{i} = x(indauxstart:indauxend);
      
 end
 
