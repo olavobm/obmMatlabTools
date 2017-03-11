95dc86b433ad4d980480936d7934bf695f38d6e1
olavobm <olavobm@gmail.com>
2017-03-10 18:31:06 -0800

    looks done, but need to add comments and deal with means.

--- obmGenDataProc/regressionSlope.m
+++ obmGenDataProc/regressionSlope.m
@@ -13,13 +13,44 @@ function a = regressionSlope(x, y)
 % mean square error between the model y* and the variable y.
 %
 % If inputs x and y are complex vectors....
-% 
 %
+% Non-zero means? More correctly: non-zero y-crossing???
+%
+% If inputs x and y are matrices...
 %
 % Olavo Badaro Marques, 10/Mar/2017.
 
-ble2 = (aaux' * baux) / (aaux' * aaux)
 
-% aaux = T7sgrid - mean(T7sgrid);
-% baux = C2sUmean - mean(C2sUmean);
-% ble2 = (aaux' * baux) / (aaux' * aaux)
+%%
+
+
+if ~iscolumn(x)
+    x = x.';   % transpose without taking the complex conjugate
+end
+
+if ~iscolumn(y)
+    y = y.';
+end
+
+% if 
+
+%%
+
+nx = size(x, 2);
+ny = size(y, 2);
+
+
+%% Compute regression slope:
+
+a = NaN(ny, nx);
+
+for i1 = 1:ny
+    
+    for i2 = 1:nx
+        
+            a(i1, i2) = (x(:, i2)' * y(:, i1)) / (x(:, i2)' * x(:, i2));
+            
+    end
+    
+end
+
