d0da6cc1dffbd5ac505e5529c33d0d665d084516
olavobm <olavobm@gmail.com>
2017-03-11 21:06:06 -0800

    added some comments.

--- obmGenDataProc/regressionSlope.m
+++ obmGenDataProc/regressionSlope.m
@@ -2,21 +2,33 @@ function a = regressionSlope(x, y)
 % a = REGRESSIONSLOPE(x, y)
 %
 %   inputs:
-%       - x:
-%       - y:
+%       - x: vector or matrix.
+%       - y: vector or matrix.
 %
 %   outputs:
-%       - a:
+%       - a: array with regression slopes (see
+%            notes below for the size of a).
 %
 % REGRESSIONSLOPE computes the regression slope a of the linear
 % model y* = a x. This constant is determined by minimizing the
 % mean square error between the model y* and the variable y.
 %
-% If inputs x and y are complex vectors....
+% If inputs x and y are vectors, then a is a single number. If one
+% is a vector and the other is a matrix, then output is a vector
+% where each element is the regression slope between the vector
+% and each row of the matrix. If both inputs are matrices, the
+% j'th column of a has the regression slopes of the j'th row of x
+% with each column of y.
+%
+% If inputs x and y are complex vectors, then a is also complex.
+% If for example the magnitude of a is 1 and its argument is pi/2,
+% then the linear model y* is a counterclockwise rotation of x
+% with no change in magnitude. Note the regression does not say
+% anything about how good (how correlated) the model is.
 %
 % Non-zero means? More correctly: non-zero y-crossing???
 %
-% If inputs x and y are matrices...
+% What about NaNs??
 %
 % Olavo Badaro Marques, 10/Mar/2017.
 
