0a9db31f57a9a753bc90575ab6976b994da7ee33
olavobm <olavobm@gmail.com>
2017-03-30 16:10:00 -0700

    updated header.

--- obmMisc/addEmptyFields2matchStructs.m
+++ obmMisc/addEmptyFields2matchStructs.m
@@ -2,16 +2,23 @@ function structoutvars = addEmptyFields2matchStructs(structvar1, structvar2)
 % structoutvars = ADDEMPTYFIELDS2MATCHSTRUCTS(structvar1, structvar2)
 %
 %   inputs:
-%       - structvar1:
-%       - structvar2:
+%       - structvar1: (scalar) structure variable.
+%       - structvar2: another (scalar) structure variable.
 %
 %   outputs:
-%       - structoutvars:
+%       - structoutvars: 1x2 structure array.
 %
-%
-%
+% When creating a structure array, all elements must have the same
+% number of fields and with the same names. ADDEMPTYFIELDS2MATCHSTRUCTS
+% compares the field names of both inputs. Fields that exist in one
+% input but not the other are added to later. The content in this newly
+% created field is an empty (double) variable.
+% 
 % Olavo Badaro Marques, 30/Mar/2017.
 
+
+%% Add empty fields to the structures
+% so they both have the same fields:
 
 fields1 = fieldnames(structvar1);
 fields2 = fieldnames(structvar2);
@@ -32,14 +39,14 @@ for i = 1:length(fieldsNotin2)
 end
 
 
-%%
+%% Assign outputs:
 
-% ...
+% Order fields.
 % This is not necessary for the concatenation below
 % (as of Matlab 2015a), but looks as good coding practice:
 structvar2 = orderfields(structvar2, fieldnames(structvar1));
 
-% Assign output
+% Assign the concatenation to output:
 structoutvars = [structvar1, structvar2];
 
 
