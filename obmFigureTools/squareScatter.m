56a81286136afd9311889f6efd6031a74c39d330
olavobm <olavobm@gmail.com>
2017-03-31 22:09:22 -0700

    edited header and comments.

--- obmFigureTools/squareScatter.m
+++ obmFigureTools/squareScatter.m
@@ -2,44 +2,51 @@ function squareScatter(x, y, margfac, lcut)
 % SQUARESCATTER(x, y, margfac, lcut)
 %
 %   inputs:
-%       -
-%       -
-%       -
-%       -
+%       - x: array of real numbers.
+%       - y: same as above
+%       - margfac (optional): percentage to extend the axes limits.
+%       - lcut (optional): NOT implemented
 %
+% Plots a scatter plot with a 1 to 1 aspect ration, which is
+% often desirable when x and y have the same dimensions. The
+% axes limits are determined by the value in x or y of largest
+% magnitude and an extra (or smaller margin) defined by margfac.
+% 
+% lcut ????? for quadrants
+%
+% Dependencies: initSquarePlot.m
 %
 %
 %
 % Olavo Badaro Marques, 16/Mar/2017.
 
-indplt = 700;
 
-p1 = [aqdp.u(indplt), aqdp.v(indplt), aqdp.w(indplt)];
-p2 = [aqdp.u(indplt+1), aqdp.v(indplt+1), aqdp.w(indplt+1)];
+%% Deal with inputs:
 
-maxx = max([abs(p1(1)), abs(p2(1))]);
-maxy = max([abs(p1(2)), abs(p2(2))]);
+% Reshape to column vectors:
+x = x(:);
+y = y(:);
 
-maxaxs = max([maxx, maxy]);
-marfac = 0.1;
+% Use default value if input margfac is not given:
+if ~exist('margfac', 'var')
+    margfac = 0.1;
+end
 
-vecaxslims = [-1 1 -1 1] .* maxaxs * (1+marfac);
 
-figure
-    plot(p1(1), p1(2), '.', 'MarkerSize', 24)
-    hold on
-    plot(p2(1), p2(2), '.', 'MarkerSize', 24)
-    axis equal
-    grid on
-    axis(vecaxslims)
-    
-    xtickslen = length(get(gca, 'XTick'));
-    ytickslen = length(get(gca, 'YTick'));
-    
-    if xtickslen > ytickslen
-        
-        set(gca, 'YTick', get(gca, 'XTick'))
-        
-    elseif xtickslen < ytickslen
-        set(gca, 'XTick', get(gca, 'YTick'))
-    end
+%% Define axes limits:
+
+maxaxs = max([max(abs(x)), max(abs(y))]);
+
+vecaxslims = [-1 1 -1 1] .* maxaxs * (1+margfac);
+
+
+%% lcut???
+
+
+%% Plot scatter plot:
+
+% Open figure (with a another function that
+% sets a few plot appearance properties):
+initSquarePlot(vecaxslims(1:2), vecaxslims(3:4))
+
+    plot(x, y, '.', 'MarkerSize', 24)
