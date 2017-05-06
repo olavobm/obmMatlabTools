fd2bc4f2b0db1617917e2709c85a055663bb6c4d
olavobm <olavobm@gmail.com>
2017-05-05 19:23:15 -0700

    implemented second input.

--- obmFigureTools/linkallaxes.m
+++ obmFigureTools/linkallaxes.m
@@ -1,19 +1,34 @@
-function linkallaxes(axsstr)
-% LINKALLAXES(axsstr)
+function linkallaxes(axsstr, haxs)
+% LINKALLAXES(axsstr, haxs)
 %
 %   inputs:
 %       - axsstr: string in the same manner as it goes into linkaxes.m
+%       - haxs (optional): vector of axes handles (if not given search
+%                          for the axes of the current figure).
 %
-% LINKALLAXES applys linkaxes.m for all axes in the current
+% LINKALLAXES apply linkaxes.m for all axes in the current
 % figure (so you don't have to specify all axes handles).
+% You can also give the axes handles to specify which axes
+% you want to link.
 %
 % Olavo Badaro Marques, 07/Mar/2017.
 
 
-%% Get all axes handles in the current figure:
+%% Get handles of axes to link:
 
-allAxes = findall(gcf, 'Type', 'axes');
-onlyAxes = allAxes(~ismember(get(allAxes, 'Tag'), {'legend', 'Colobar'}));
+% If second input is not given, get current figure's axes handles:
+if ~exist('haxs', 'var')
+    
+    % Get all axes handles in the current figure:
+    allAxes = findall(gcf, 'Type', 'axes');
+    onlyAxes = allAxes(~ismember(get(allAxes, 'Tag'), {'legend', 'Colobar'}));
+   
+% Otherwise use input:
+else
+   
+    onlyAxes = haxs;
+    
+end
 
 
 %% Call linkaxes:
