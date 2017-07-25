08ef2b17ee817a4991be182832e12b91aa417e5d
olavobm <olavobm@gmail.com>
2017-07-25 16:52:44 -0700

    updated header.

--- obmFigureTools/textFig.m
+++ obmFigureTools/textFig.m
@@ -2,17 +2,20 @@ function htxt = textFig(x0, y0, s, varargin)
 % htxt = TEXTFIG(x0, y0, s, varargin)
 %
 %   inputs:
-%       - x0:
-%       - y0:
-%       - s:
-%       - varargin (optional):
+%       - x0: x position of the text on the figure (from 0 to 1).
+%       - y0: y position.
+%       - s: string to be written on the figure.
+%       - varargin (optional): any parameter/value that can
+%                              go into the text.m function.
 %
 %   outputs:
-%       - htxt:
+%       - htxt: text handle.
 %
 %
 % Olavo Badaro Marques, 18/Jul/2017
 
+
+%%
 
 haxs = axes('Position', [0 0 1 1], 'Visible', 'off');
 
