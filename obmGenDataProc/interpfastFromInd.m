52b2ab13c85f4324e6f49de95e6fe4ef49102f88
olavobm <olavobm@gmail.com>
2017-07-24 17:20:59 -0700

    implemented the calculation.

--- obmGenDataProc/interpfastFromInd.m
+++ obmGenDataProc/interpfastFromInd.m
@@ -2,21 +2,32 @@ function y0 = interpfastFromInd(x, y, x0, ind4interp)
 % y0 = INTERPFASTFROMIND(x, y, x0, ind4interp)
 %
 %   inputs:
-%       -
-%       -
-%       -
-%       -
+%       - x:
+%       - y:
+%       - x0:
+%       - ind4interp:
 %
 %   outputs:
-%       -
+%       - y0:
 %
 %
 %
 % Olavo Badaro Marques, 24/Jul/2017.
 
 
+%%
 
-lOK = ~isnan(ind1);
+y0 = NaN(1, size(y, 2));
 
-y0(lOK) = x(ind1(lOK)) + (x0 - y(ind1(lOK))) .* ...
-          ((x(ind2(lOK)) - x(ind1(lOK))) ./ (y(ind2(lOK)) - y(ind1(lOK))));
+%
+lok = ~isnan(ind4interp(1, :));
+y0(ind4interp(1, lok)) = y(ind4interp(1, lok));
+
+%
+ind1 = ind4interp(2, :);
+ind2 = ind1 + 1;
+lok = ~isnan(ind1);
+
+y0(lok) = y(ind1(lok)) + (x0 - x(ind1(lok))) .* ...
+                    ((x(ind2(lok)) - x(ind1(lok))) ./ ...
+                     (y(ind2(lok)) - y(ind1(lok))));
