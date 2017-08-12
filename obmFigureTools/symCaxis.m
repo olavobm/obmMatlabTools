a903f81688a19923e0e79307e64bf54ce9828d34
olavobm <olavobm@gmail.com>
2017-08-11 18:57:19 -0700

    added handling multiple axes handles.

--- obmFigureTools/symCaxis.m
+++ obmFigureTools/symCaxis.m
@@ -1,21 +1,31 @@
 function symCaxis(haxs)
-%
+% SYMCAXIS(haxs)
 %
 %   inputs
-%       -
-%
-%   outputs
-%       -
+%       - haxs:
 %
 %
+%
+% TO DO:
+%       - Add option to set limits of multiple axes to the same range.
 %
 % Olavo Badaro Marques, 11/Aug/2017.
 
+
+%%
 
 if ~exist('haxs', 'var')
     haxs = gca;
 end
 
-valmax = max(abs(haxs.CLim));
 
-haxs.CLim = valmax .* [-1, 1];
+%%
+
+for i = 1:length(haxs)
+    
+    valmax = max(abs(haxs(i).CLim));
+
+    haxs(i).CLim = valmax .* [-1, 1];
+end
+
+
