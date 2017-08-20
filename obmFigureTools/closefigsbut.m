c84ede804dfd98bfce81034c69ccd1900fae6f79
olavobm <olavobm@gmail.com>
2017-08-19 17:19:43 -0700

    edited comments.

--- obmFigureTools/closefigsbut.m
+++ obmFigureTools/closefigsbut.m
@@ -2,23 +2,25 @@ function closefigsbut(fignumbers)
 % CLOSEFIGSBUT(fignumbers)
 %
 %   inputs
-%       - fignumbers
+%       - fignumbers: array of figure numbers.
 %
-%
-%
-%
-%
-%
+% CLOSEFIGSBUT close all the figures but
+% those with numbers given in the input.
 %
 % Olavo Badaro Marques, 18/Aug/2017.
 
 
+%% Get all figure numbers (groot was implemented in Matlab's
+% version 2014b -- substitute by 0 for earlier versions)
+
+% Get all figure handles
 allFigHnds = findall(groot, 'Type', 'Figure');
 
+% Get correspondent figure numbers
 allFigNumbers = [allFigHnds(:).Number];
 
 
-%%
+%% Loop over figure numbers and close if not present in input
 
 for i = 1:length(allFigNumbers)
     
