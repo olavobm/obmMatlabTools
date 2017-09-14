e052479b4deec8b55016c804b1b1a5e35320bb6e
olavobm <olavobm@gmail.com>
2017-09-13 23:42:02 -0700

    initial commit.

---
+++ obmFiltering/bandPassFilter.m
@@ -0,0 +1,15 @@
+function yfilt = bandPassFilter(x, y, fs, nord)
+% yfilt = BANDPASSFILTER(x, y, fs, nord)
+%
+%   inputs
+%       - x:
+%       - y:
+%       - fs:
+%       - nord:
+%
+%   outputs
+%       - yfilt:
+%
+%
+%
+% Olavo Badaro Marques, 14/Sep/2017.
