c5e182a3f5927f78eaf1784edcde190017b5d6c9
olavobm <olavobm@gmail.com>
2017-10-12 11:09:46 -0700

    implemented changing the aspect ratio of existing axes.

--- obmFigureTools/initAxesAspectRatio.m
+++ obmFigureTools/initAxesAspectRatio.m
@@ -1,15 +1,21 @@
-function initAxesAspectRatio(xlims, ylims, aspecrto)
-% initAxesAspectRatio(xlims, ylims, aspecrto)
+function haxs = initAxesAspectRatio(xlims, ylims, aspecrto, haxs)
+% haxs = initAxesAspectRatio(xlims, ylims, aspecrto, haxs)
 %
-%   inputs:
-%       -
-%       -
+%   inputs
+%       - xlims:
+%       - ylims:
 %       - aspecrto (optional): length by height. If not specified, the
 %                              option axis equal is set.
+%       - haxs (optional): axes handle. If not specified, create a new
+%                          figure with only one axes.
+%
+%   outputs
+%       - haxs:
 %
 % Function INITAXESFREEZEASPECTRATIO written to create axes with a certain
 % aspect ratio, which is necessary when plotting a vector field (see
-% function plotArrows.m).
+% function plotArrows.m). Input haxs allows this function to change the
+% aspect ratio of existing axes.
 %
 % There might be some small (hopefully small) uncertainties due to small
 % differences in the aspect ratio of the figure and of the axes.
@@ -22,25 +28,43 @@ function initAxesAspectRatio(xlims, ylims, aspecrto)
 
 
 %%
+
 if length(xlims)~=2 || length(ylims)~=2
     error('Specified axes limits have more than 2 elements')
+end
+
+%
+if ~exist('haxs', 'var') || isempty(haxs)
+	lnewfig = true;
+else
+    lnewfig = false;
 end
 
 
 %%
 
-figure     % change this later, for
-    axes   % example, to allow subplots
+if lnewfig
+    figure
+        haxs = axes;
+end
+
+
+%%
+
+% If there is no input for the aspect ratio, set axis equal:
+if ~exist('aspecrto', 'var') || isempty(aspecrto)
+    axis equal
+
+% Otherwise, set input-specified aspect ratio:
+else
+    pbaspect(haxs, [aspecrto, 1, 1])
+end
+
+axis([xlims, ylims])
     
-    % If there is no input for the aspect ratio, set axis equal:
-    if ~exist('aspecrto', 'var') || isempty(aspecrto)
-        axis equal
-        
-	% Otherwise, set input-specified aspect ratio:
-    else
-        pbaspect([aspecrto 1 1])
-    end
     
-    axis([xlims, ylims])
+
+    
+    
     
 % setFigSize(lenhgt, units)   % most likely not necessary...
