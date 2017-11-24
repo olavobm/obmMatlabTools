e8954c16489dfed08735954631272be70deb1396
olavobm <olavobm@gmail.com>
2017-11-23 22:34:18 -0800

    edited header.

--- obmFigureTools/plot3Dprof.m
+++ obmFigureTools/plot3Dprof.m
@@ -1,19 +1,35 @@
-function hplt = plot3Dprof(z, u, v, varargin)
-% hplt = PLOT3DPROF(z, u, v, varargin)
+function hplt = plot3Dprof(u, v, z, varargin)
+% hplt = PLOT3DPROF(u, v, z, varargin)
 %
 %   inputs
-%       -
-%       -
-%       -
+%       - u: x component to plot.
+%       - v: y     "      "   " .
+%       - z: vertical location of the vectors (u, v).
 %       -
 %
 %   outputs
-%       -
+%       - hplt: handles of the plot (or vertices of the fill plot)
 %
 %
 %
 %
+% Olavo Badaro Marques, 23/Nov/2017.
+
+
+%% Set default options for some possible parameter of varargin
+
 %
+p = inputParser;
+
+%
+defltPlot = true;
+
+%
+addParameter(p, 'Plot', defltPlot)
+
+% Fill variable p with default values or input specifications:
+parse(p, varargin{:})
+
 
 %%
 
@@ -48,14 +64,28 @@ zplt(3:4, :) = [z2'; z2'];
 
 %%
 
-hplt = fill3(uplt, vplt, zplt, [0.5, 0.5, 0.5]);
 
-%
-dataAspectRto = daspect;
-daspect([dataAspectRto(1), dataAspectRto(1), dataAspectRto(3)]);
+if p.Results.Plot
+    
+    
+    hplt = fill3(uplt, vplt, zplt, [0.5, 0.5, 0.5]);
+
+    %
+    dataAspectRto = daspect;
+    daspect([dataAspectRto(1), dataAspectRto(1), dataAspectRto(3)]);
+    
+else
+    
+    uvzaux = cat(3, uplt, vplt);
+    
+    hplt = cat(3, uvzaux, zplt);
+      
+
+end
 
 
 %%
+
 if nargout==0
     clear hplt
 end
