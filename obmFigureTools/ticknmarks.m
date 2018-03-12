0f28f1b0396488f30c10b9a3be5eca4a49137635
olavobm <olavobm@gmail.com>
2018-03-12 12:50:06 -0700

    wrote some comments.

--- obmFigureTools/ticknmarks.m
+++ obmFigureTools/ticknmarks.m
@@ -6,7 +6,15 @@ function ticknmarks(xyzstr, nticks, varargin)
 %       - nticks:
 %       - varargin (optional):
 %
-%
+% TICKNMARKS creates evenly spaced ticks for an axis on an
+% axes handle. The current axis limit is used to create the
+% evenly spaced ticks and both limits are included in the
+% set of ticks. You can also change the ticks for multiple
+% axes (x, y and z) or on multiple axes handles.
+% 
+% 
+% 
+% 
 % TO DO:
 %   - select decimal place
 %   - convert datenum to datestr
@@ -35,7 +43,7 @@ else
 end
 
 
-%%
+%% Look at input "xyzstr" to see what axes will be ticked:
 
 %
 tickaxsStr = cell(1, 3);
@@ -57,22 +65,25 @@ end
 tickaxsStr = tickaxsStr(~cellfun(@isempty, tickaxsStr));
 
 
-%%
+%% Create variables to map from an axis ('x', 'y', 'z')
+% to their correspondent properties in an axes handle:
 
 tickaxsMap = containers.Map({'x', 'y', 'z'}, {'XTick', 'YTick', 'ZTick'});
 limsaxsMap = containers.Map({'x', 'y', 'z'}, {'XLim', 'YLim', 'ZLim'});
 
 
-%%
+%% Set axis' ticks:
 
+% Loop over axes handles
 for i1 = 1:length(haxs)
     
+    % Loop over axes to tick
     for i2 = 1:length(tickaxsStr)
         
-        %
+        % Get limits of the current axis
         axslims = get(haxs(i1), limsaxsMap(tickaxsStr{i2}));
         
-        %
+        % Create evenly spaced ticks on the current axis
         set(haxs(i1), tickaxsMap(tickaxsStr{i2}), ...
                       linspace(axslims(1), axslims(2), nticks))
         
