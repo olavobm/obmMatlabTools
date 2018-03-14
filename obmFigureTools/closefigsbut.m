87eff8fd985259215d7955e6df4c9f4796cf945e
olavobm <olavobm@gmail.com>
2018-03-14 15:23:52 -0700

    added option of no input.

--- obmFigureTools/closefigsbut.m
+++ obmFigureTools/closefigsbut.m
@@ -4,8 +4,10 @@ function closefigsbut(fignumbers)
 %   inputs
 %       - fignumbers: array of figure numbers.
 %
-% CLOSEFIGSBUT close all the figures but
-% those with numbers given in the input.
+% CLOSEFIGSBUT close all the figures but those with
+% numbers given in the input. You can also not give
+% any input, in which case only the current active
+% figure will not be closed.
 %
 % Olavo Badaro Marques, 18/Aug/2017.
 
@@ -18,6 +20,15 @@ allFigHnds = findall(groot, 'Type', 'Figure');
 
 % Get correspondent figure numbers
 allFigNumbers = [allFigHnds(:).Number];
+
+
+%% If there is no input, then get number of current figure
+% such that this is the only one that will not be closed
+
+if nargin==0
+    hfig = gcf;
+    fignumbers = hfig.Number;
+end
 
 
 %% Loop over figure numbers and close if not present in input
