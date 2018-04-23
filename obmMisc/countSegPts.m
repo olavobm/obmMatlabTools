003931e792686aafd5a2dd722851b74f41b570a6
olavobm <olavobm@gmail.com>
2018-04-23 14:50:40 -0700

    Initial commit.

---
+++ obmMisc/countSegPts.m
@@ -0,0 +1,16 @@
+function [] = countSegPts(tlims, t, x)
+%
+%
+%   inputs
+%       - tlims:
+%       - t:
+%       - x:
+%
+%   outputs
+%       -
+%
+%
+% Output: number, mean tlim, 
+%
+%
+% Olavo Badaro Marques, 23/Apr/2018.
