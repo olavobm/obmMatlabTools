9977346e86173774ea6560b15a5f651c06af9ec3
olavobm <olavobm@gmail.com>
2018-04-23 15:19:33 -0700

    added the rest of the code to make this function functional.

--- obmMisc/countPtsSegs.m
+++ obmMisc/countPtsSegs.m
@@ -12,14 +12,39 @@ function [nptsperseg] = countPtsSegs(tlims, t, x)
 %
 % Output: number, mean tlim, (in seg and in seg no-NaN)
 %
+% See also: makeSegs.m
+%
 % Olavo Badaro Marques, 23/Apr/2018.
 
 
 %%
+%
+% need to improve this/make sure that input tlims is good
 
+if isvector(tlims)
+    
+    %
+    binlims = NaN(length(tlims)-1, 2);
+    
+    binlims(:, 1) = tlims(1:end-1);
+    binlims(:, 2) = tlims(2:end);
+    
+else
+    
+    
+end
 
 
 %%
+
+if iscolumn(t)
+	t = t.';
+end
+
+
+if iscolumn(x)
+	x = x.';
+end
 
 
 %%
@@ -29,13 +54,13 @@ nptsperseg = createEmptyStruct({'meanbin', 'npts', 'nptsok'});
 
 %%
 
-nptsperseg.meanbin = mean(tlims, 2);
+nptsperseg.meanbin = mean(binlims, 2);
 
 
 %%
 
 nr = size(x, 1);
-nsegs = NaN;
+nsegs = size(binlims, 1);
 
 
 %%
@@ -50,8 +75,8 @@ for i1 = 1:nr
     for i2 = 1:nsegs
         
         %
-        linseg = (t(i1, :) >= tlims(i1, 1)) & ...
-                 (t(i1, :) <  tlims(i1, 2));
+        linseg = (t(i1, :) >= binlims(i1, 1)) & ...
+                 (t(i1, :) <  binlims(i1, 2));
         
         %
         nptsperseg.npts(i1, i2) = length(find(linseg));
