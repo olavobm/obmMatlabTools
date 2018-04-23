7a7e0f1972e754c7adebaccfa2ffb9fc9c89417c
olavobm <olavobm@gmail.com>
2018-04-23 15:07:39 -0700

    wrote basic code of the function.

--- obmMisc/countPtsSegs.m
+++ obmMisc/countPtsSegs.m
@@ -1,5 +1,5 @@
-function [] = countSegPts(tlims, t, x)
-%
+function [nptsperseg] = countPtsSegs(tlims, t, x)
+% [nptsperseg] = COUNTPTSSEGS(tlims, t, x)
 %
 %   inputs
 %       - tlims:
@@ -7,10 +7,61 @@ function [] = countSegPts(tlims, t, x)
 %       - x:
 %
 %   outputs
-%       -
+%       - nptsperseg:
 %
 %
-% Output: number, mean tlim, 
-%
+% Output: number, mean tlim, (in seg and in seg no-NaN)
 %
 % Olavo Badaro Marques, 23/Apr/2018.
+
+
+%%
+
+
+
+%%
+
+
+%%
+
+nptsperseg = createEmptyStruct({'meanbin', 'npts', 'nptsok'});
+
+
+%%
+
+nptsperseg.meanbin = mean(tlims, 2);
+
+
+%%
+
+nr = size(x, 1);
+nsegs = NaN;
+
+
+%%
+
+%
+nptsperseg.npts = NaN(nr, nsegs);
+nptsperseg.nptsok = NaN(nr, nsegs);
+
+%
+for i1 = 1:nr
+    
+    for i2 = 1:nsegs
+        
+        %
+        linseg = (t(i1, :) >= tlims(i1, 1)) & ...
+                 (t(i1, :) <  tlims(i1, 2));
+        
+        %
+        nptsperseg.npts(i1, i2) = length(find(linseg));
+             
+        %
+        lnoNaN = ~isnan(x(i1, linseg));
+        nptsperseg.nptsok(i1, i2) = length(find(lnoNaN));
+        
+    end
+    
+end
+
+
