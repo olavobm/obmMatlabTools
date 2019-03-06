62ad988c643e28297fa8bebcf49360e6d0f243b5
olavobm <olavobm@gmail.com>
2019-03-05 18:40:30 -0800

    now the first input can be a vector of axes handles.

--- obmFigureTools/shadeAll.m
+++ obmFigureTools/shadeAll.m
@@ -2,17 +2,19 @@ function shadeAll(h2shd, strtype)
 % SHADEALL(h2shd, strtype)
 %
 %   inputs
-%       - h2shd:
-%       - strtype:
+%       - h2shd (optional): figure OR axes handles (vector) to shade
+%                           (do NOT mix figure and axes handle together).
+%       - strtype (optional): string setting the shading option.
 %
 %
-%
+% So far, this only does shading flat and works with no inputs.
 %
 % Olavo Badaro Marques, 07/Feb/2019.
 
 
 %%
 
+%
 if nargin == 0
     
     %
@@ -29,9 +31,39 @@ if nargin == 0
         end
     end
     
-    
+%
 else
     
+	%
+    if isa(h2shd(1), 'matlab.graphics.axis.Axes')
+        
+        %
+        Naxs = length(h2shd);
+        haxs2shd = h2shd;
+        
+	%
+    elseif isa(h2shd(1), 'matlab.ui.Figure')
+        % NOT DONE!!!!
+        %
+        allhaxs_cell = cell(1, length(h2shd));
+        
+        %
+        for i = 1:length(h2shd)
+            allhaxs_cell{i} = h2shd(i).Children;
+        end
+        
+        % Now I need to concatenate all
+        % allhaxs_cell into an array
+        
+	%
+    else
+        error('Not allowed.')
+    end
+    
+    %
+    for i = 1:Naxs
+        shading(haxs2shd(i), 'flat')
+    end
     
     
 end
