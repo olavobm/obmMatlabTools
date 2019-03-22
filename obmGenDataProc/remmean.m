f7ea6c75a255089e015059586d3ea133a8d0287b
olavobm <olavobm@gmail.com>
2019-03-21 17:19:35 -0700

    added comments.

--- obmGenDataProc/remmean.m
+++ obmGenDataProc/remmean.m
@@ -2,26 +2,32 @@ function xout = remmean(xin, dim)
 % xout = REMMEAN(xin, dim)
 %
 %   inputs
-%       - xin:
-%       - dim:
+%       - xin: a double array (1D, 2D, N-D).
+%       - dim: dimension along which to calculate the mean.
 %
 %   outputs
-%       - xout:
+%       - xout: double array the same size as xin.
 %
+% REMMEAN.m is a simple function to remove the mean
+% of xin along the dimension dim. The purpose of this
+% function is to use repmat consistently without having
+% to write it over and over again.
 %
 % Olavo Badaro Marques, 21/Mar/2019.
 
-%
+
+%%
+
+% Get length in the appropriate direction
 Nrep = size(xin, dim);
 
-%
+% Use the above to create the vector
+% for repeating the "mean array"
 vec_rep = ones(1, ndims(xin));
-
-%
 vec_rep(dim) = Nrep;
 
-%
+% Calculate the mean along the dimension "dim"
 x_mean = mean(xin, dim);
 
-%
+% Remove x_mean from the array xin
 xout = xin - repmat(x_mean, vec_rep);
