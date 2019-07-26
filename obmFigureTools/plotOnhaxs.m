47aa1f87cc6100586bce3e58a00f06a531ecb466
olavobm <olavobm@gmail.com>
2019-07-26 14:28:41 -0700

    initial commit.

---
+++ obmFigureTools/plotOnhaxs.m
@@ -0,0 +1,37 @@
+function plotOnhaxs(haxs, x, y, xctr, yctr, scale, rotang, varargin)
+% PLOTONHAXS(haxs, x, y, xctr, yctr, scale, rotang, varargin)
+%
+%   inputs
+%       -
+%       -
+%       -
+%       -
+%       -
+%       -
+%       -
+%
+%   outputs
+%       -
+%
+%
+%
+% Olavo Badaro Marques, 26/Jul/2019.
+
+
+%%
+
+%
+xlimaux = haxs.XLim;
+ylimaux = haxs.YLim;
+
+%
+xlimrange = diff(xlimaux);
+ylimrange = diff(ylimaux);
+
+
+%%
+
+
+%%
+
+plot(haxs(1), xplt, ypl)
