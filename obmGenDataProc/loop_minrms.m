77e99f9cc80e807d7396241eb95c80fb1dbe662d
olavobm <olavobm@gmail.com>
2019-07-26 18:24:41 -0700

    organized output and improved header.

--- obmGenDataProc/loop_minrms.m
+++ obmGenDataProc/loop_minrms.m
@@ -1,20 +1,41 @@
 function minrmsoutstrct = loop_minrms(xg, yg, xd, yd, x0xendxn)
-% minrmsoutstrct = LOOP_AND_MINRMS(xg, yg, yd, dxd, x0dxloop)
+% minrmsoutstrct = LOOP_MINRMS(xg, yg, yd, dxd, x0xendxn)
 %
 %   inputs
-%       - xg:
-%       - yg:
-%       - xd:
-%       - yd:
-%       - x0dxxend: 1x3 vector with the initial location, 
+%       - xg: a vector with the grid (independent variable)
+%             of a variable yg.
+%       - yg: a data vector (with same length as xg).
+%       - xd: a vector with the independent variable of the data yd.
+%       - yd: data vector.
+%       - x0xendxn: 1x3 vector with the initial location, 
 %                   ...... , respectively.
 %
 %   outputs
 %       - struct variable with the fields:
-%           *
-%           *
-%           *
-%           *
+%           * corr4x:
+%           * xdcorr:
+%           * xg:
+%           * yg:
+%           * allxd:
+%           * allyd:
+%           * allyg:
+%           * allrms:
+%           * ind_minrms:
+%           * minrmsoutstrct.minrms
+%
+% 
+%
+% LOOP_MINRMS.m compares yd with several "realizations" of yg
+% (as determined by input x0xendxn) and find the correspondent
+% x (xdcorr) correspondent with the minimum root-mean-square
+% (RMS) error. In essence, LOOP_MINRMS.m is like a regression
+% of yd against yg to find the best bias correction to xd.
+%
+% I originally wrote this function for a very specific problem:
+% to estimate the distance between a ship and a towed instrument
+% based on depth/altimeter data from the instrument and a bathymetry
+% databased.
+%
 %
 % x need to be sorted!
 %
@@ -59,8 +80,6 @@ allx0 = x0xendxn(1) : x0xendxn(3) : x0xendxn(2);
 
 
 
-
-
 %%
 %
 xdiff_aux = repmat(xdiff_vec, 1, length(allx0));
@@ -91,8 +110,10 @@ ydmatrix = repmat(yd_vec(:), 1, length(allx0));
 %%
 
 %
-yddev = ydmatrix - repmat(nanmean(ydmatrix, 1), ndpts, 1);
-ygdev = all_yg - repmat(nanmean(all_yg, 1), ndpts, 1);
+% % yddev = ydmatrix - repmat(nanmean(ydmatrix, 1), ndpts, 1);
+% % ygdev = all_yg - repmat(nanmean(all_yg, 1), ndpts, 1);
+yddev = ydmatrix;
+ygdev = all_yg;
 
 %
 all_misfits = yddev - ygdev;
@@ -109,11 +130,20 @@ all_rms = sqrt(mean(all_misfits.^2, 1));
 %% Organize output
 
 %
-minrmsoutstrct.corr4x = xd_vec(1) - all_x4loop(1, ind_minrms);
+minrmsoutstrct.corr4x = all_x4loop(1, ind_minrms) - xd_vec(1);
 minrmsoutstrct.xdcorr = xd_vec + minrmsoutstrct.corr4x;
 
 %
-minrmsoutstrct.xgbest = all_x4loop(:, ind_minrms);
+minrmsoutstrct.xg = xg;
+minrmsoutstrct.yg = yg;
+
+%
+minrmsoutstrct.allxd = all_x4loop;
+minrmsoutstrct.allyd = ydmatrix;
+minrmsoutstrct.allyg = all_yg;
+
+%
+% % minrmsoutstrct.allxg = all_x4loop;
 
 %
 minrmsoutstrct.allrms = all_rms;
